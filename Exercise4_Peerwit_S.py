04ddc50c6584c757bb6a89457ff7d8ca93df7822
vaccine2805 <peerawit.suktorn@gmail.com>
2023-03-19 18:47:50 +0700

    Finish Ex

---
+++ Exercise4_Peerwit_S.py
@@ -0,0 +1,10 @@
+eng = 70
+bu = 65
+cs = 80
+cp = 75
+
+print("---Your Score---")
+print("Foundation Endlish :", eng, "Points")
+print("General Business :", eng, "Points")
+print("Introduction to Computer Systems :", eng, "Points")
+print("Computer Programming :", eng, "Points")
